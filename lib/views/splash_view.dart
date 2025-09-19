8300de8f91aadfad9fee7547e7bcabc52fc2d215
Sabry Waleed <https://github.com/SabriWaleed>
2025-09-19 18:36:40 +0300

    Commit_Splash_View

---
+++ lib/views/splash_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'login_view.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginView()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          "lib/assets/images/logo.png",
+          width: 140.w,
+          height: 140.h,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
